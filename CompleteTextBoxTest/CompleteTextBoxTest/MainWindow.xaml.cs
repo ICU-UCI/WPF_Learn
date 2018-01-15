40207b5da1a2469cfa3f03f205d766b25a839dc8
GR <13249937090@163.com>
2018-01-15 21:23:46 +0800

    TextBox输入文字后自动弹出数据
    实现TextBox输入文字后自动弹出数据(类似百度的输入框)

---
+++ CompleteTextBoxTest/CompleteTextBoxTest/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CompleteTextBoxTest
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            textBox1.AddItem(new AutoCompleteEntry("上海", null));
+            textBox1.AddItem(new AutoCompleteEntry("北京", null));
+            textBox1.AddItem(new AutoCompleteEntry("济南", null));
+            textBox1.AddItem(new AutoCompleteEntry("青岛", null));
+            textBox1.AddItem(new AutoCompleteEntry("天津", null));
+            textBox1.AddItem(new AutoCompleteEntry("黑龙江", null));
+            textBox1.AddItem(new AutoCompleteEntry("聊城", null));
+            textBox1.AddItem(new AutoCompleteEntry("聊城1", null));
+            textBox1.AddItem(new AutoCompleteEntry("聊城2", null));
+            textBox1.AddItem(new AutoCompleteEntry("聊城3", null));
+        }
+    }
+}
